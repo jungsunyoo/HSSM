3103ae80183e013ab2a8bc8ad629ea47a136f92d
yoojungsun0 <yoojungsun0@gmail.com>
2025-08-21 01:19:56 -0600

    running actual reg on pni

--- docs/tutorials/hssm-tst-reg-gpu.py
+++ docs/tutorials/hssm-tst-reg-gpu.py
@@ -367,7 +367,7 @@ def main():
 
     outfile = os.path.join(
         args.outdir,
-        f"model1_mb_ssc{args.ssc}_idata_chain{args.chain_id}.nc"
+        f"model1_mb_reg_ssc{args.ssc}_idata_chain{args.chain_id}.nc"
     )
     az.to_netcdf(idata, outfile)
     print(f"Saved ssc {args.ssc} chain {args.chain_id} to {outfile}")
